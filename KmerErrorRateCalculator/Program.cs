2003cc854f5383bfd86cde4ead209b3da406458a
Raichi Nakanishi <m@raichi.nl>
2021-07-12 06:00:38 +0900

    update

--- KmerErrorRateCalculator/Program.cs
+++ KmerErrorRateCalculator/Program.cs
@@ -14,25 +14,52 @@ namespace KmerErrorRateCalculator
         static void Main(string[] args)
         {
             var k = Int32.Parse(args[0]);
-            outputPath += k.ToString();
-            string fastqPath = args[1];
-            var Reads = GetReadsFromFastq(fastqPath);
-            //var b = GetKmerCount(k, Reads.First().Gene,DnaBase);
-            var b = new List<output>();
-            Reads.ForEach(x => b.Add(new output() {KmerCounts = GetKmerCount(k, x.Gene, DnaBase),Name = x.ReadName }));
-            int i = 1;
-            b.ForEach(x =>
+            var max = Int32.Parse(args[1]);
+            for (k = k; k <= max; k++)
             {
-                Console.WriteLine($"Generating csv{i} of {b.Count}...");
-                var csv = "";
-                foreach (var y in x.KmerCounts)
+                Console.WriteLine($"begin {k} of {max}");
+                var filterCount = 10000;
+                string fastqPath = args[2];
+                var Reads = GetReadsFromFastq(fastqPath);
+                var b = new List<output>();
+                Reads.Where(x => x.Length >= filterCount).ToList().ForEach(x => b.Add(new output() { KmerCounts = GetKmerCount(k, x.Gene, DnaBase), Name = x.ReadName, Quality = x.QualityAverage, Length = x.Length }));
+
+                //RPK of reads
+                var o = new Dictionary<string, double>();
+                foreach (var x in GetKmerList(k, DnaBase))
                 {
-                    csv += y.Key + "," + y.Value.ToString() + "\n";
+                    var count = 0;
+                    var length = 0;
+                    foreach (var y in b)
+                    {
+                        count += y.KmerCounts[x];
+                        length += y.Length;
+                    }
+                    o.Add(x, (double)((count / (double)length) ) * 1000);
+                }
+                Console.WriteLine($"finished calculate read's RPK ({k}/{max})");
+
+                //参照配列のRPK
+                string referenceFilePath = args[3];
+                var sq = File.ReadAllLines(referenceFilePath);
+                var referenceString = "";
+                for (int j = 1; j < sq.Length; j++)
+                {
+                    referenceString += sq[j];
+                }
+                var referenceChars = referenceString.ToCharArray();
+                var kmerCount = GetKmerCount(k, referenceChars, DnaBase);
+                var csvTitle = "flowcellVer,basecallerNameVer,k,kmer,refRPK,readRPK\n";
+                var csv = "";
+                foreach (var x in kmerCount)
+                {
+                    csv += "," + "," + k.ToString() + "," + x.Key + "," + ((double)((x.Value/ (double)referenceChars.Length) ) * 1000).ToString() + "," + o[x.Key] + "\n";
                 }
                 if (!File.Exists(outputPath)) Directory.CreateDirectory(outputPath);
-                File.WriteAllText($"{outputPath}/{x.Name}.csv", csv);
-                i++;
-            });
+                File.WriteAllText($"{outputPath}/RPK-{k}.csv", csvTitle + csv);
+                if (!File.Exists($"{outputPath}/RPK.csv")) File.WriteAllText($"{outputPath}/RPK.csv", csvTitle);
+                File.AppendAllText($"{outputPath}/RPK.csv",csv);
+            }   
             Console.WriteLine("Complete!");
         }
 
@@ -53,19 +80,19 @@ namespace KmerErrorRateCalculator
             }
             return reads;
         }
-        static Dictionary<string, int> GetKmerCount(int k,char[] gene,char[] bases)
+        static Dictionary<string, int> GetKmerCount(int k, char[] gene, char[] bases)
         {
             var a = GetKmerList(k, bases);
             var counter = new Dictionary<string, int>();
             a.ForEach(x => counter.Add(x, 0));
-            for (int i = 0;i <= gene.Length - k; i++)
+            for (int i = 0; i <= gene.Length - k; i++)
             {
                 var b = "";
-                for(int j = 0; j < k; j++)
+                for (int j = 0; j < k; j++)
                 {
                     b += gene[i + j];
                 }
-                counter[b]++; 
+                counter[b]++;
             }
             return counter;
         }
@@ -85,11 +112,11 @@ namespace KmerErrorRateCalculator
         static List<string> GetKmerList(int k, char[] bases)
         {
             var a = new List<string>();
-            for(int i = 0; i < k; i++)
+            for (int i = 0; i < k; i++)
             {
-                if(i == 0)
+                if (i == 0)
                 {
-                    foreach(var x in bases)
+                    foreach (var x in bases)
                     {
                         a.Add(x.ToString());
                     }
@@ -105,6 +132,8 @@ namespace KmerErrorRateCalculator
     {
         public Dictionary<string, int> KmerCounts { get; set; }
         public string Name { get; set; }
+        public double Quality { get; set; }
+        public int Length { get; set; }
         public output()
         {
             KmerCounts = new Dictionary<string, int>();
